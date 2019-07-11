27c08f93cf62fd4afc4757985f30c81ad83c17ba
Vivek <vajbe.tce@gmail.com>
2019-07-11 21:17:40 +0530

    changed response details

--- server/controllers/providerController.js
+++ server/controllers/providerController.js
@@ -2,14 +2,20 @@ let providerModel = require('../models/provider');
 
 exports.view = function (req, res) {
     res.setHeader("Content-Type", "application/json");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    
     providerModel.find((err, items) => {
         if (err) {
             console.log("Error while retrieving the data => " + err);
         }
         else if (items.length > 0) {
-            res.send(items);
+            res
+            .status(200)
+            .send(items);
         }
         else {
+            res.status(200);
             res.send(JSON.parse('{"response" : "No providers available."}'));
         }
     });
