786b36c3ce18bf8962d5769fed8addcd216d45a9
Vivek <vajbe.tce@gmail.com>
2019-07-11 22:31:47 +0530

    Added header

--- server/controllers/providerController.js
+++ server/controllers/providerController.js
@@ -4,6 +4,7 @@ exports.view = function (req, res) {
     res.setHeader("Content-Type", "application/json");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     
     providerModel.find((err, items) => {
         if (err) {
