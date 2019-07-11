3d740a0b22157e74c09e40d33958305ad6b4883b
Vivek <vajbe.tce@gmail.com>
2019-07-11 20:18:21 +0530

    Adding cors

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const apiRoutes = require(__dirname + '/server/routes/api-routes');
 const dbConn = require(__dirname + "/server/config/creds");
 const app = express();
+const cors = require('cors');
 
 
 let port = process.env.PORT || 5566;
@@ -16,6 +17,8 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use('/api',apiRoutes); 
+
+app.use(cors());
 
 let url = `mongodb+srv://${dbConn.user}:${dbConn.password}@${dbConn.host}/${dbConn.db}?retryWrites=true`;
 
